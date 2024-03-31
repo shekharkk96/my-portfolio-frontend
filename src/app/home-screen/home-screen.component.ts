8562a8b2219445ffa513d6e96c2c7dbbd1941219
shekharkk96 <shekharkk2010@gmail.com>
2024-03-31 20:34:11 +0530

    created coming soon page wit countdown

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-screen.component.scss']
 })
 export class HomeScreenComponent implements OnInit {
+  intervalId : any;
+  countDown : string = '';
+  finalDate = new Date("April 30, 2024 12:00:00").getTime();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.startCountDown();
+  }
+
+  calculateCountDown() : void{
+    const presentDate = new Date().getTime();
+    const distance = this.finalDate - presentDate;
+
+    if(distance < 0){
+      this.countDown = "Wait for little more"
+    }else{
+      const days = Math.floor(distance/(1000*60*60*24));
+      const hours = Math.floor((distance % (1000*60*60*24))/(1000*60*60));
+      const minute = Math.floor((distance % (1000*60*60))/(1000*60));
+      const second = Math.floor((distance % (1000*60))/(1000));
+      this.countDown = days + "d " + hours + "h " + minute + "m " + second+ "s";
+    }
+  }
+
+  startCountDown(): void{
+    this.intervalId = setInterval(() =>{
+      this.calculateCountDown();
+    },1000);
   }
 
 }
